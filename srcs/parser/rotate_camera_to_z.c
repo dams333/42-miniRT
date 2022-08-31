c1cdabda9a4b0302b86b92e26ddc3ec73166cd3e
Jeremy Maia <jmaia@student.42.fr>
2022-08-31 11:46:04 +0200

    WIP

--- srcs/parser/rotate_camera_to_z.c
+++ srcs/parser/rotate_camera_to_z.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 18:50:33 by jmaia             #+#    #+#             */
-/*   Updated: 2022/08/30 19:59:53 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/08/31 11:25:31 by jmaia            ###   ###               */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,19 @@ void	rotate_camera_to_z(t_i_3d_vec camera_vec, t_list *pos)
 
 static void	rotate_object(t_i_3d_vec *pos, t_i_3d_vec camera_vec)
 {
-	// Ici faut appliquer ce qui est noté dans le carnet magique
+	double	dist;
+	double	point_angle;
+	double	rotate_angle;
+	double	new_angle;
+
+	dist = get_distance(0, 0, *pos->x, *pos->y);
+	point_angle = get_angle(*pos->x, *pos->y);
+	rotate_angle = get_angle(*camera_vec.x, *camera_vec.y);
+	new_angle = point_angle + rotate_angle;
+	*pos->x = cos(new_angle) * dist;
+	*pos->y = sin(new_angle) * dist;
+	// Il reste plus qu'à le refaire encore une fois sur le 2e axe pour l'avoir en 3D
+	// Et en vrai pour ça, go faire une fonction qui rotate toujours en 2D et appeler 2 fois cette fonction ici
 }
 
 static double	get_distance(double x1, double y1, double x2, double y2)
@@ -42,5 +54,15 @@ static double	get_distance(double x1, double y1, double x2, double y2)
 
 static double	get_angle(double x, double y)
 {
-	
+	double	base_angle;
+
+	base_angle = atan(abs(y) / abs(x));
+	if (x > 0 && y > 0)
+		return (base_angle);
+	if (x < 0 && y > 0)
+		return (PI - base_angle);
+	if (x < 0 && y < 0)
+		return (PI + base_angle);
+	if (x > 0 && y < 0)
+		return (2*PI - base_angle);
 }

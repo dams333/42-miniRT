f1f9b41683399b581f747c32ceb508b7894476d5
Jeremy Maia <jmaia@student.42.fr>
2022-09-06 16:16:20 +0200

    J'AI MES BOULES

--- srcs/render/render.c
+++ srcs/render/render.c
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 19:20:14 by jmaia             #+#    #+#             */
-/*   Updated: 2022/09/04 20:41:47 by jmaia            ###   ###               */
+/*   Updated: 2022/09/06 13:20:42 by jmaia            ###   ###               */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 static t_list	*extract_spheres(t_generic_object *object_list);
 static void		render_pixel(t_img *canvas, int c_x, int c_y, t_list *spheres);
 static double	calc_delta(double v_x, double v_y, double v_z, t_sphere_object *s);
-static void		put_pixel(t_img *canvas, int x, int y, int r, int g, int b);
+static void		put_pixel(t_img *canvas, int x, int y, int color);
 
 void	render(t_img *canvas, t_generic_object *object_list)
 {
@@ -68,21 +68,23 @@ static void	render_pixel(t_img *canvas, int c_x, int c_y, t_list *spheres) // Sp
 {
 	double			v_x;
 	double			v_y;
+	double			v_height;
 	t_list			*cur;
 	t_sphere_object	*cur_sphere;
 	double			delta;
 
-	v_x = 1.0 * c_x / canvas->width;
-	v_y = 1.0 * c_y / canvas->height;
+	v_height = 1.0 * canvas->height / canvas->width;
+	v_x = 1.0 * c_x / canvas->width - 0.5;
+	v_y = v_height * c_y / canvas->height - v_height / 2;
 	cur = spheres;
 	while (cur)
 	{
 		cur_sphere = (t_sphere_object *) cur->content;
 		delta = calc_delta(v_x, v_y, 1, cur_sphere);
 		if (delta > 0)
-			put_pixel(canvas, c_x, c_y, 0, 0, 0);
+			put_pixel(canvas, c_x, c_y, 0x00FF0000);
 		else
-			put_pixel(canvas, c_x, c_y, 1, 0, 0);
+			put_pixel(canvas, c_x, c_y, 0);
 		cur = cur->next;
 	}
 }
@@ -92,28 +94,21 @@ static double	calc_delta(double v_x, double v_y, double v_z, t_sphere_object *s)
 	double	a;
 	double	b;
 	double	c;
+	double	delta;
 
 	a = pow(v_x, 2) + pow(v_y, 2) + pow(v_z, 2);
 	b = -2 * (v_x * s->coord_x + v_y * s->coord_y + v_z * s->coord_z);
 	c = pow(s->coord_x, 2) + pow(s->coord_y, 2) + pow(s->coord_z, 2) - pow(s->diameter / 2, 2);
 	
+	delta = pow(b, 2) - 4 * a * c;
 	return (pow(b, 2) - 4 * a * c);
 }
 
-static void	put_pixel(t_img *canvas, int x, int y, int r, int g, int b)
+static void	put_pixel(t_img *canvas, int x, int y, int color)
 {
-	char	*data;
+	int		*data;
 	int		junk;
 
-	(void) r;
-	(void) g;
-	(void) b;
-	data = (char *) mlx_get_data_addr(canvas, &junk, &junk, &junk);
-	data[(y * canvas->width + x) * 4 + 0] = 0x00;
-	data[(y * canvas->width + x) * 4 + 1] = 0x00;
-	if (x == 0)
-		data[(y * canvas->width + x) * 4 + 2] = 0xFF;
-	else
-		data[(y * canvas->width + x) * 4 + 2] = 0x00;
-	data[(y * canvas->width + x) * 4 + 3] = 0x00;
+	data = (int *) mlx_get_data_addr(canvas, &junk, &junk, &junk);
+	data[(y * canvas->width + x)] = color;
 }

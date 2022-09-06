f64e982cf8c08dc50276ed53e53452092307e960
Jeremy Maia <jmaia@student.42.fr>
2022-09-06 16:57:42 +0200

    WIP apply rotate

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: dhubleur <dhubleur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 17:19:23 by dhubleur          #+#    #+#             */
-/*   Updated: 2022/09/04 17:44:38 by jmaia            ###   ###               */
+/*   Updated: 2022/09/06 16:33:52 by jmaia            ###   ###               */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,11 @@
 
 void	render(t_img	*canvas, t_generic_object *object_list);
 static void	fill_camera_pos(t_i_3d_vec *camera_pos, t_generic_object *object_list);
+static void	fill_camera_vec(t_i_3d_vec *camera_vec, t_generic_object *object_list);
 static void	fill_pos(t_list **pos, t_generic_object *object_list);
+
+void	center_camera(t_i_3d_vec camera_pos, t_list *pos);
+void	rotate_camera_to_z(t_i_3d_vec camera_vec, t_list *pos);
 
 int	main(int argc, char **argv)
 {
@@ -38,12 +42,14 @@ int	main(int argc, char **argv)
 	
 	t_img		*img;
 	t_i_3d_vec	camera_pos;
+	t_i_3d_vec	camera_vec;
 	t_list		*pos;
 
 	fill_camera_pos(&camera_pos, object_list);
+	fill_camera_vec(&camera_vec, object_list);
 	fill_pos(&pos, object_list);
-//	center_camera(camera_pos, pos);
-//	rotate_camera_to_z();
+	center_camera(camera_pos, pos);
+	rotate_camera_to_z(camera_vec, pos);
 	img = mlx_new_image(mlx.mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
 	render(img, object_list);
 	mlx_put_image_to_window(mlx.mlx, mlx.win, img, 0, 0);
@@ -68,6 +74,26 @@ static void	fill_camera_pos(t_i_3d_vec *camera_pos, t_generic_object *object_lis
 			camera_pos->x = &cam->coord_x;
 			camera_pos->y = &cam->coord_y;
 			camera_pos->z = &cam->coord_z;
+			return;
+		}
+		cur = cur->next;
+	}
+}
+
+static void	fill_camera_vec(t_i_3d_vec *camera_vec, t_generic_object *object_list)
+{
+	t_generic_object	*cur;
+	t_camera_object		*cam;
+
+	cur = object_list;
+	while (cur)
+	{
+		if (cur->type == CAMERA)
+		{
+			cam = (t_camera_object *) cur->specific_object;
+			camera_vec->x = &cam->orientation_x;
+			camera_vec->y = &cam->orientation_y;
+			camera_vec->z = &cam->orientation_z;
 			return;
 		}
 		cur = cur->next;

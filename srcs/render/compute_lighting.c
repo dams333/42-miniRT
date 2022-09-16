f8a68df9ec8816ab2a966c2639b1d0789e39a072
Jeremy Maia <jmaia@student.42.fr>
2022-09-16 12:53:28 +0200

    WIP

--- srcs/render/compute_lighting.c
+++ srcs/render/compute_lighting.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   compute_lighting.h                                 :+:      :+:    :+:   */
+/*   compute_lighting.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 16:45:48 by jmaia             #+#    #+#             */
-/*   Updated: 2022/09/10 16:52:33 by jmaia            ###   ###               */
+/*   Updated: 2022/09/13 21:09:00 by jmaia            ###   ###               */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,17 @@ double	compute_lighting(t_i_3d_vec *point, double ambient, t_list *lights)
 static double	compute_single_lighting(t_i_3d_vec *point,
 											t_light_object *cur_light)
 {
-	(void) point;
-	(void) cur_light;
+	double	light_spread;
+
+	light_spread = get_light_spread(point, cur_light);
+	return (cur_light->brightness_ratio / light_spread);
+}
+
+static double	get_light_spread(t_i_3d_vec *point, t_light_object *cur_light)
+{
+	double	light_spread;
+	double	light_ray_angle;
+
+	light_ray_angle = atan(point.y / point.x)
+	light_spread = cur_light->brightness_ratio / sin(light_ray_angle);
 }
